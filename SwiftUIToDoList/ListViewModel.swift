3f6c724f8e85315bbad467701a6026b3024633cd
Caroline Davis <caroline_davis@live.com>
2022-02-18 11:36:55 +1100

    feat: Save items

--- SwiftUIToDoList/ListViewModel.swift
+++ SwiftUIToDoList/ListViewModel.swift
@@ -7,21 +7,39 @@
 
 import Foundation
 
+/* CRUD
+ Create
+ Read
+ Update
+ Delete
+ */
+
 class ListViewModel: ObservableObject {
 
-    @Published var items: [ItemModel] = []
+    // did set gets called anytime anytime we change this items array
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
 
     init() {
         getItems()
     }
 
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "This is the second title", isCompleted: true),
-            ItemModel(title: "This is the third title", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+
+        // unpack the json data
+        // we want it to be the type of [itemModel] not an actual [itemModel] so we put .self
+        // can combine both these into 1 guard let instead of 2
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+
+        self.items = savedItems
+
     }
 
     // as onDelete needs an indexSet we can use this easily
@@ -43,6 +61,13 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+
+    // generally this type of data would be saved in coredata but this is how you save stuff in userdefaults. need to convert data to json
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.setValue((encodedData), forKey: itemsKey)
         }
     }
 
